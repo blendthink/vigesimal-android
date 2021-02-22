9873dce32f4f6dab5d8f348150652df59d1ae104
blendthink <info@blendthink.dev>
2021-02-22 21:06:38 +0900

    add shared-release.aar

--- app/src/main/java/dev/honwakalab/vigesimal/android/MainActivity.kt
+++ app/src/main/java/dev/honwakalab/vigesimal/android/MainActivity.kt
@@ -2,10 +2,15 @@ package dev.honwakalab.vigesimal.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dev.honwakalab.vigesimal.lib.shared.VigesimalConverter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val decimal = VigesimalConverter.toDecimal("ja")
+        Log.d("blendthink", decimal.toPlainString())
     }
 }
