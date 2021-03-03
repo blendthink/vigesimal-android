9b54a12302b4e91c17640c45e4497fedc5fba9b9
blendthink <info@blendthink.dev>
2021-03-03 09:34:22 +0900

    create convertButton click event

--- app/src/main/java/dev/honwakalab/vigesimal/android/MainActivity.kt
+++ app/src/main/java/dev/honwakalab/vigesimal/android/MainActivity.kt
@@ -1,7 +1,10 @@
 package dev.honwakalab.vigesimal.android
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import com.google.android.material.snackbar.Snackbar
 import dev.honwakalab.vigesimal.android.databinding.ActivityMainBinding
 import dev.honwakalab.vigesimal.lib.shared.VigesimalConverter
 
@@ -11,8 +14,29 @@ class MainActivity : AppCompatActivity() {
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
 
-        val decimal = VigesimalConverter.toDecimal("ja")
-        binding.result.text = decimal.toPlainString()
+        binding.convertButton.setOnClickListener {
+
+            // hide keyboard
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus?.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+
+            val inputText = binding.input.text.toString()
+            if (inputText.isBlank()) {
+                binding.result.text = null
+                return@setOnClickListener
+            }
+
+            try {
+                val decimal = VigesimalConverter.toDecimal(inputText)
+                binding.result.text = decimal.toPlainString()
+            } catch (e: IllegalArgumentException) {
+                Snackbar.make(
+                        binding.root,
+                        R.string.snackbar_text_failed_convert,
+                        Snackbar.LENGTH_SHORT,
+                ).show()
+            }
+        }
 
         setContentView(binding.root)
     }
